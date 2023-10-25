1102602dd6aca52bfcfb80237264441049653f91
Lider Máximo <llopezraccioppe@danielcastelao.org>
2023-10-25 10:48:33 +0200

    Veryfy

--- app/src/main/java/com/dam/mvvm/MainActivity.kt
+++ app/src/main/java/com/dam/mvvm/MainActivity.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.ColorRes
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -19,6 +20,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.dam.mvvm.ui.theme.MvvmTheme
@@ -111,7 +113,9 @@ class MainActivity : ComponentActivity() {
                         .width((300 / 2).dp)
                         .padding(horizontal = 30.dp, vertical = 0.dp),
                     colors = ButtonDefaults.buttonColors(Color.Gray)) {
-                    Text(text = "Sumar Ronda")
+                    Image(
+                        painter = painterResource(R.drawable.very),
+                        contentDescription = "Icono2")
                 }
 
             }
